78198985f903fd4d083a3f5fa1861891ad91b149
grayhams74s <felixjunang@gmail.com>
2026-01-24 14:51:04 +0800

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by felix angcot jr on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
