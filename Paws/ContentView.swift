9c632239311a21025a040b36d5fc0d9f96171858
grayhams74s <felixjunang@gmail.com>
2026-02-07 18:14:58 +0800

    push

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -6,19 +6,159 @@
 //
 
 import SwiftUI
+import SwiftData
+import TipKit
+
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @Environment(\.modelContext) var modelContext
+    @Query private var pets: [Pet]
+    
+    @State private var path = [Pet] ()
+    @State private var isEditing: Bool = false
+    
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120))
+    ]
+    
+    let buttonTip = ButtonTip()
+    
+    func setUpTips(){
+        do {
+            // This command reset the tips data store to its initial state to the initial state
+            //for retesting purpose
+            try Tips.resetDatastore()
+            // This will customize often our tip presented on our app after displaying another tip
+            try Tips.configure([
+                .displayFrequency(.daily)
+            ])
+        }catch {
+            print("Error initilization Tip Kit \(error.localizedDescription)")
         }
-        .padding()
+    }
+    
+    init() {
+        setUpTips()
+    }
+    
+    func addPet() {
+        let pet = Pet(name: "Best Friend")
+        modelContext.insert(pet)
+        path = [pet]
+    }
+    
+    
+    var body: some View {
+        NavigationStack(path: $path){
+            ScrollView{
+                LazyVGrid(columns: layout){
+                    GridRow{
+                        ForEach(pets) { pet in
+                            NavigationLink(value: pet){
+                                VStack {
+                                    
+                                    if let imageData = pet.photo {
+                                        if let image = UIImage(data: imageData){
+                                            Image(uiImage: image)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(minWidth: 0, maxWidth: .infinity)
+                                                .clipShape(RoundedRectangle(cornerRadius: 20, style: .circular))
+                                        }
+                                        
+                                        // Placeholder image if no valid photo
+                                    } else {
+                                        Image(systemName: "pawprint.circle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(40)
+                                            .foregroundStyle(.quaternary)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Text(pet.name)
+                                        .font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    
+                                    Spacer()
+                                }// : VStack
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                .background(pet.photo == nil ? AnyShapeStyle(.ultraThinMaterial) : AnyShapeStyle(Color.white))
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                                .overlay(alignment: .topTrailing){
+                                    if isEditing {
+                                        Menu {
+                                            Button("Delete", systemImage: "trash", role: .destructive){
+                                                modelContext.delete(pet)
+                                                try? modelContext.save()
+                                            }
+                                        } label: {
+                                            Image(systemName: "trash.circle.fill")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: 36, height: 36)
+                                                .foregroundStyle(.red)
+                                                .symbolRenderingMode(.multicolor)
+                                                .padding(4)
+                                            
+                                        }
+                                    }
+                                }
+                                
+                            }// :NAVLINK
+                            // This code changes the link color from blue to primary color.
+                            .foregroundStyle(.primary)
+                        }// : LOOP
+                    }//: GRID ROW
+                }// : GRID LAYOUT
+                .padding(.horizontal) // As you can see in the preview, this horizontal padding enhances our design even further
+                EmptyView()
+            }//: ScrollView
+            .navigationTitle(pets.isEmpty ? "" : "Paws")
+            .navigationDestination(for: Pet.self) { pet in
+                EditPetView(pet: pet)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            isEditing.toggle()
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        addPet()
+                    })
+                    {
+                        Image(systemName: "plus.circle")
+                            .foregroundStyle(Color.blue)
+                        
+                    }
+                    .popoverTip(buttonTip)
+                    
+                    // We import TipKit to use this properties of TipKit
+                }
+            }
+            .overlay {
+                if pets.isEmpty {
+                    CustomContentUnavailableView(icon: "dog.circle", title: "No Pets", description: "Add a new pet to get started.")
+                }
+            }
+        } //:STACK
     }
 }
 
-#Preview {
+#Preview("Sample Data"){
     ContentView()
+        .modelContainer(Pet.preview)
+}
+
+#Preview ("No Data"){
+    ContentView()
+        .modelContainer(for: Pet.self, inMemory: true)
 }
--- Paws/PawsApp.swift
+++ Paws/PawsApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct PawsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+            // This code sets the model container in this view to store the provided model type
+            // It also create a new container if necessary, and sets a model context for it in this view's environment
+                .modelContainer(for: Pet.self)
         }
     }
 }
